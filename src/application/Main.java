f720db7bf7eaad2ab1d2029aa9287213d64a15d8
Nekuin <Nekuin@gmail.com>
2019-01-28 14:32:04 +0200

    test

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			//asd
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
