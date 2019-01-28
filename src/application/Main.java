f10b3318f653dccb15b7ef3a4964b0418d8dedff
Tuomas Makela <tuomas.makela@metropolia.fi>
2019-01-28 14:47:47 +0200

    Bracnh-test . koko muutettu 800,400

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 		try {
 			//asd
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,200,400);
+			Scene scene = new Scene(root,800,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
