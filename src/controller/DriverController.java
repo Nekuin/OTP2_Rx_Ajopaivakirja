c78b15c59065effd4d634e632f601c110936e148
Jori Nordlund <jori.nordlund@metropolia.fi>
2019-01-28 15:11:51 +0200

    DriverController lisätty ja interfaceen metodeja.

---
+++ src/controller/DriverController.java
@@ -0,0 +1,29 @@
+package controller;
+
+public class DriverController implements DriverControllerIF {
+
+	@Override
+	public long getStartingTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getFinishingTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getStartingPoint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getFinishingPoint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/controller/DriverControllerIF.java
+++ src/controller/DriverControllerIF.java
@@ -1,5 +1,10 @@
 package controller;
 
 public interface DriverControllerIF {
+	public abstract long getStartingTime();
+	public abstract long getFinishingTime();
+	public abstract String getStartingPoint();
+	public abstract String getFinishingPoint();
+	
 
 }
