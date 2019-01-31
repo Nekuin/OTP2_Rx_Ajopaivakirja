20feb04433867526b453b28e4db5f241a2c8d8d7
Nekuin <Nekuin@gmail.com>
2019-01-31 15:39:37 +0200

    hibernate cfg ja session factory

--- src/model/AjoAccessObject.java
+++ src/model/AjoAccessObject.java
@@ -1,9 +1,27 @@
 package model;
 import java.sql.Connection;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 //DAO
 
 public class AjoAccessObject {
 	
-
+	SessionFactory istuntotehdas= null;
+	
+	public AjoAccessObject() {
+		
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+		try{
+			istuntotehdas = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		}
+		catch(Exception e){
+			System.out.println("Oh no");
+			StandardServiceRegistryBuilder.destroy( registry );
+			e.printStackTrace();
+		}
+	}
 }
