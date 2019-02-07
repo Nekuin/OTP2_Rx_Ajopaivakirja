a1b9a7bf9b6b600e3cdad14acab7992ca92dc1d1
Nekuin <Nekuin@gmail.com>
2019-02-07 15:13:08 +0200

    test to default maven dir

---
+++ src/test/DriverTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import model.*;
+
+class DriverTest {
+
+	@Test
+	@DisplayName("Make driver")
+	void createDriver() {
+		IDriver driver = new Driver("Kalle", 1, "AB");
+		AjoAccessObject a = new AjoAccessObject();
+		
+		boolean test = a.createDriver((Driver)driver);
+		
+		assertEquals(true, test, "Creating the driver failed!");
+	}
+
+}
