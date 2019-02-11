2a022fe1e74069e6456014883341f7f04cb365af
Jori Nordlund <jori.nordlund@metropolia.fi>
2019-02-11 15:12:07 +0200

    Hr manager modified

--- src/model/HrManager.java
+++ src/model/HrManager.java
@@ -1,17 +1,32 @@
 package model;
 
 public class HrManager extends Employee {
+	
+	private AjoAccessObject a;
 
+	/**
+	 * Constructor for then hr manager
+	 * @param name name of the hr manager
+	 * @param employeeID id of the hr manager
+	 */
 	public HrManager(String name, int employeeID) {
 		super(name, employeeID);
 	}
 	
-	public void addDriver(String name, int employeeID, String driversLicense) {
-		Driver driver = new Driver(name, employeeID, driversLicense);
+	/**
+	 * 
+	 * @param name
+	 * @param employeeID
+	 * @param driversLicense
+	 */
+	public boolean addDriver(String name, int employeeID, String driversLicense) {
+		IDriver driver = new Driver(name, employeeID, driversLicense);
+		return a.createDriver(driver);
+		
 	}
 	
 	public void removeDriver(int employeeID) {
-		
+		a.deleteDriver(employeeID);
 	}
 	
 	public void addDrivingShift(int shiftID, String startTime, String finishTime, Driver shiftDriver) {
