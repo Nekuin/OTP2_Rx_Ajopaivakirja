64482e55a01f2e78e0d335147c61da02fd4b5b94
Tuomas Makela <tuomas.makela@metropolia.fi>
2019-02-12 14:43:46 +0200

    Hr-manager class changes and HRManagerTest class created.

--- test/employee/HRManagerTest.java
+++ test/employee/HRManagerTest.java
@@ -12,6 +12,16 @@ public class HRManagerTest {
 	
 	static HrManager testHR;
 	
+<<<<<<< Upstream, based on branch 'Development' of https://github.com/Nekuin/OTP1-R6.git
+=======
+	@BeforeAll
+	static void setup() {
+		System.out.println("testing");
+		a = new AjoAccessObject();
+		Driver arska = new Driver("Arska Ajaja", 1002, "C1");
+	}
+
+>>>>>>> df0916a Hr-manager class changes and HRManagerTest class created.
 	@BeforeEach
 	void resetHR() {
 		testHR = new HrManager("HR Heikki");
@@ -23,4 +33,22 @@ public class HRManagerTest {
 		assertEquals("HR Heikki", testHR.getName());
 	}
 	
+<<<<<<< Upstream, based on branch 'Development' of https://github.com/Nekuin/OTP1-R6.git
+=======
+	@Test
+	@DisplayName("Test removeDriver")
+	void removeDriver() {			
+		boolean test = testHR.removeDriver(1002);//ei toimi
+		assertEquals(true, test, "Removing the driver failed!");
+	}
+	
+	@Test
+	@DisplayName("Test addDrivingShift")
+	void addDrivingShift() {			
+	//	boolean test = testHR.addDrivingShift(101, "12:00", "16:00", arska);
+		//assertEquals(true, test, "Removing the driver failed!");
+	}
+	//addDrivingShift
+	//removeDrivingShift
+>>>>>>> df0916a Hr-manager class changes and HRManagerTest class created.
 }
