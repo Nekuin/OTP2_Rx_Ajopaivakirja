b1de1ae7d8b0921e76a734effdf43a93f56e5436
Jori Nordlund <jori.nordlund@metropolia.fi>
2019-02-26 15:56:29 +0200

    controller modified, goodShifts changed to arraylist

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -71,7 +72,7 @@ public class Controller implements IController{
 	
 	public List<IDrivingShift> readGoodDrivingShifts(IDriver driver) {
 		List<IDrivingShift> shifts = this.drivingShiftAO.readDrivingShift().stream().collect(Collectors.toList());
-		List<IDrivingShift> goodShifts = null;
+		List<IDrivingShift> goodShifts = new ArrayList<IDrivingShift>();
 		boolean drivable;
 		
 		if(driver.getCanDriveHazardous() == false) {
