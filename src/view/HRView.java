66a05fb9bb1b4a948bf1ba1fba566ef1e70f30b1
Jori Nordlund <jori.nordlund@metropolia.fi>
2019-03-08 20:32:14 +0200

    HrView modified, added driver info and remove driver button

--- src/view/HRView.java
+++ src/view/HRView.java
@@ -5,9 +5,13 @@ import java.util.Collection;
 import controller.IController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import model.Driver;
 
@@ -28,6 +32,24 @@ public class HRView {
 	public GridPane driverInfo() {
 		GridPane grid = new GridPane();
 		Text title = new Text("Drivers:");
+		
+		VBox driverBox = new VBox();
+		driverBox.setPadding(new Insets(10));
+		
+		Text driverInfo = new Text("Driver Info: ");
+		Text name = new Text("");
+		Text driversLicense = new Text("");;
+		Text driverID = new Text("");;
+		
+		
+		Button deleteDriver = new Button("Remove");
+		deleteDriver.setOnAction(e -> {
+			this.controller.deleteDriver(lv.getSelectionModel().getSelectedItem());
+		});
+		
+		
+		
+		driverBox.getChildren().addAll(driverInfo, name, driverID, driversLicense, deleteDriver);
 
 		drivers = FXCollections.observableArrayList();
 		lv = new ListView<>();
@@ -35,14 +57,22 @@ public class HRView {
 
 		lv.setOnMouseClicked(e -> {
 			Driver clicked = lv.getSelectionModel().getSelectedItem();
+			name.setText(clicked.getName());
+			driversLicense.setText(clicked.getDriversLicense());
+			driverID.setText(Integer.toString(clicked.getEmployeeID()));
 
 		});
-
+		
+		
+		
 		grid.add(title, 0, 0);
 		grid.add(lv, 0, 1);
+		grid.add(driverBox, 1, 1);
 
 		return grid;
 	}
+	
+	
 
 	public BorderPane getHRView() {
 		return this.bpane;
