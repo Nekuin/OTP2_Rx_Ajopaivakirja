c4dccc1470609feabdf08f2d30eaf34de37aa18f
Simo Ojala/Nekuin <Simo.ojala@metropolia.fi>
2019-04-15 21:46:57 +0300

    Removed SessionFactory related methods from HibernateUtil, use EntityManager instead

--- src/util/HibernateUtil.java
+++ src/util/HibernateUtil.java
@@ -1,60 +1,16 @@
 package util;
 
-import java.util.Properties;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.*;
-import org.hibernate.service.*;
-
 public class HibernateUtil {
 
 	
-	private static SessionFactory sessionFactory;
 	private static EntityManagerFactory emf;
 	private static EntityManager em;
 	
-	/**
-	 * Creates a session factory
-	 * @return SessionFactory sessionFactory
-	 */
-	private static SessionFactory buildSessionFactory() {
-		try {
-			Configuration configuration = new Configuration();
-			configuration.configure("hibernate.cfg.xml");
-			configuration.addAnnotatedClass(model.Driver.class);
-			configuration.addAnnotatedClass(model.DrivingShift.class);
-			configuration.addAnnotatedClass(model.Client.class);
-			configuration.addAnnotatedClass(model.Cargo.class);
-			configuration.addAnnotatedClass(model.Superior.class);
-			configuration.addAnnotatedClass(model.HrManager.class);
-			System.out.println("Configuration loaded");
-			
-			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-			System.out.println("serviceregistry created");
-			
-			SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-			
-			return sessionFactory;
-		}
-		catch(Throwable e) {
-			System.err.println("sessionfactory creation failed. " + e);
-			throw new ExceptionInInitializerError(e);
-		}
-	}
-	/**
-	 * Getter for the session factory, if it does not exist, creates one
-	 * @return SessionFactory sessionFactory
-	 */
-	
-	public static synchronized SessionFactory getSessionFactory() {
-		if(sessionFactory == null) sessionFactory = buildSessionFactory();
-		return sessionFactory;
-	}
 	/**
 	 * Getter for an entity manager factory
 	 * @return EntityManagerFactory emf
