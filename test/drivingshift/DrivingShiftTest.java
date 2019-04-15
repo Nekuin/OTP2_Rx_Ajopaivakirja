881ce07bf3a604a0b8c4a61b51dab7fa197eeb37
Simo Ojala/Nekuin <Simo.ojala@metropolia.fi>
2019-04-15 21:19:33 +0300

    database test for DrivingShift

--- test/drivingshift/DrivingShiftTest.java
+++ test/drivingshift/DrivingShiftTest.java
@@ -1,22 +1,37 @@
 package drivingshift;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import controller.Controller;
+import controller.IController;
+import model.Cargo;
+import model.Client;
 import model.Driver;
 import model.DrivingShift;
 
 public class DrivingShiftTest {
 	static DrivingShift testShift;
 	static Driver testDriver;
+	static IController controller;
 	
 	@BeforeAll
 	public static void setup() {
 		testDriver = new Driver("Kalle", "AB");
+		//we don't have an instance of view => null as first parameter
+		//use false as the second if you want to test locally
+		//and use true if you want Jenkins to test
+		controller = new Controller(null, true);
 	}
 	
 	@BeforeEach
@@ -58,5 +73,14 @@ public class DrivingShiftTest {
 	void shiftDriver() {
 		testShift.setShiftDriver(testDriver);
 		assertEquals(testDriver, testShift.getShiftDriver(), "Shift driver is not correct!");
+	}
+	
+	@Test
+	@DisplayName("DrivingShift DB test")
+	void createDrivingShift() {
+		DrivingShift s = new DrivingShift(new Client(), new Cargo(), LocalDate.now());
+		controller.createDrivingShift(s);
+		List<DrivingShift> shifts = controller.readAllDrivingShifts();
+		assertTrue(shifts.contains(s));
 	}
 }
