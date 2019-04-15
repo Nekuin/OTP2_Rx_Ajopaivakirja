797783b3c89342ae0bd63b244425cc0c53d8fa36
Simo Ojala/Nekuin <Simo.ojala@metropolia.fi>
2019-04-15 21:54:30 +0300

    Removed couple more obsolete AccessObjects (use typed Dao class)
    Removed unsused IDao interface, I don't think we need this.

--- src/model/HrManager.java
+++ src/model/HrManager.java
@@ -1,19 +1,13 @@
 package model;
-
-import java.time.LocalDate;
-
 import javax.persistence.Entity;
-import javax.persistence.Transient;
+
 /**
  * Class for the HrManager
- * @author Nekuin
+ * @author Nekuin, Jori
  *
  */
 @Entity
 public class HrManager extends Employee{
-	
-	@Transient
-	private DrivingShiftAO ao;
 	
 	/**
 	 * Constructor for the hr manager which takes the name of the HrManager as parameter
@@ -29,34 +23,6 @@ public class HrManager extends Employee{
 	 */
 	public HrManager() {
 		super();
-	}
-	
-	
-	/**
-	 * Sets the access object for driving shift objects
-	 * @param ao driving shift access object
-	 */
-	public void setDrivingShiftAO(DrivingShiftAO ao) {
-		this.ao = ao;
-	}
-	
-	/**
-	 * Adds driving shift to the database
-	 * @param client client of the driving shift
-	 * @param cargo cargo of the driving shift
-	 */
-	public void addDrivingShift(Client client, Cargo cargo, LocalDate deadline) {
-		DrivingShift shift = new DrivingShift(client, cargo, deadline );
-		ao.createDrivingShift(shift);
-	}
-	
-	
-	/**
-	 * Removes driving shift from the database
-	 * @param shift shift that is about to be removed
-	 */
-	public void removeDrivingShift(DrivingShift shift) {
-		ao.delete(shift);
 	}
 
 }
