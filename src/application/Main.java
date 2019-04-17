594d244fea9e2525300221198b27c3215f8cca57
Simo Ojala/Nekuin <Simo.ojala@metropolia.fi>
2019-04-17 12:52:05 +0300

    re-interrupting Hibernate startup thread as per https://inno-jenkinsnew.educloud.metropolia.fi/sonarqube/issues?open=AWog2Gczyt-G9GQmPK9R&resolved=false&types=BUG

--- src/application/Main.java
+++ src/application/Main.java
@@ -97,6 +97,7 @@ public class Main extends Application implements IView {
 						Thread.sleep(100);
 					} catch (InterruptedException e1) {
 						e1.printStackTrace();
+						Thread.currentThread().interrupt();
 					}
 				
 				//createViews must be called on UI thread!!
