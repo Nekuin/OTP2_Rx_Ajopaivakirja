8dba76051f44881db4f7cbd4b967953d6fc30935
Jori Nordlund <jori.nordlund@metropolia.fi>
2019-04-18 13:35:24 +0300

    Hr view modified, couple driver tests added

--- test/employee/DriverTest.java
+++ test/employee/DriverTest.java
@@ -7,17 +7,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import controller.Controller;
+import controller.IController;
 import model.*;
 
 public class DriverTest {
 	
 	
 	static Driver testdriver;
+	static IController controller;
 
 	
 	@BeforeAll
 	public static void setup() {
 		System.out.println("testing");
+		controller = new Controller(null,true);
 	}
 	
 	@BeforeEach
@@ -45,5 +49,19 @@ public class DriverTest {
 		testdriver.setDriversLicense("C1");
 		assertEquals("C1", testdriver.getDriversLicense(), "Drivers license has not changed!");
 	}
+	
+	@Test
+	@DisplayName("Can drive hazardous?")
+	void canDriveHazardous() {
+		assertEquals(false,testdriver.getCanDriveHazardous(),"This driver should not be able to drive hazardous!");
+	}
+	
+	@Test
+	@DisplayName("Set can drive hazardous")
+	void setCanDriveHazardous() {
+		testdriver.setCanDriveHazardous(true);
+		assertEquals(true,testdriver.getCanDriveHazardous(),"This driver should be able to drive hazardous!");
+	}
+	
 	
 }
