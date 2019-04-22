f44100d522f0d5c26d24291c3fbd0429efa9af45
Simo Ojala/Nekuin <Simo.ojala@metropolia.fi>
2019-04-22 15:34:18 +0300

    split AddDriverModal setup function into many smaller functions

--- src/view/AddDriverModal.java
+++ src/view/AddDriverModal.java
@@ -35,38 +35,63 @@ public class AddDriverModal {
 	private void setup() {
 		
 		canDriveHazardous = false;
-		// label
+
+		// button box
+		HBox buttons = new HBox();
+		buttons.setSpacing(30);
+		buttons.setPadding(new Insets(30, 30, 30, 30));
+		buttons.getChildren().addAll(createConfirmButton(), createCancelButton());
+
+		GridPane pane = new GridPane();
+		pane.add(createTitle(), 0, 0);
+		pane.add(createDriverNameBox(), 0, 1);
+		pane.add(createDriversLicenseBox(), 0, 2);
+		pane.add(createHazardousInfoBox(), 0, 3);
+		pane.add(buttons, 0, 4);
+		
+		modalPane.setBottom(pane);
+	}
+	
+	private VBox createTitle() {
 		VBox labelBox = new VBox();
 		labelBox.setSpacing(30);
 		labelBox.setPadding(new Insets(30, 20, 20, 20));
 		Text label = new Text(strings.getString("add_driver_button"));
 		labelBox.getChildren().add(label);
-
-		// drivers name
+		return labelBox;
+	}
+	
+	private HBox createDriverNameBox() {
 		HBox driverNameBox = new HBox();
 		driverNameBox.setSpacing(20);
 		driverNameBox.setPadding(new Insets(20, 20, 20, 20));
 		Text nameText = new Text(strings.getString("drivers_name"));
 		driverNameTextF = new TextField();
 		driverNameBox.getChildren().addAll(nameText, driverNameTextF);
-
-		// drivers license
+		return driverNameBox;
+	}
+	
+	private HBox createDriversLicenseBox() {
 		HBox driversLicenseBox = new HBox();
 		driversLicenseBox.setSpacing(20);
 		driversLicenseBox.setPadding(new Insets(20, 20, 20, 20));
 		Text licenseText = new Text(strings.getString("licence_input"));
 		driversLicenseTextF = new TextField();
 		driversLicenseBox.getChildren().addAll(licenseText, driversLicenseTextF);
-
-		// hazarouds info
+		return driversLicenseBox;
+	}
+	
+	private HBox createHazardousInfoBox() {
 		HBox hazardousInfoBox = new HBox();
 		hazardousInfoBox.setSpacing(20);
 		hazardousInfoBox.setPadding(new Insets(20, 20, 20, 20));
 		Text hazardousInfo = new Text(strings.getString("hazardous_input"));
 		hazaBox = new CheckBox();
 		hazardousInfoBox.getChildren().addAll(hazardousInfo, hazaBox);
-
-		// confirm button
+		return hazardousInfoBox;
+	}
+	
+	private Button createConfirmButton() {
 		Button addDriver = new Button(strings.getString("submit_button_text"));	
 		addDriver.setOnAction(e -> {
 			Driver newDriver = new Driver(driverNameTextF.getText(), driversLicenseTextF.getText(), canDriveHaz());
@@ -75,28 +100,15 @@ public class AddDriverModal {
 			driversLicenseTextF.setText("");
 			((Node) e.getSource()).getScene().getWindow().hide();
 		});
-
-		// cancel button
+		return addDriver;
+	}
+	
+	private Button createCancelButton() {
 		Button cancelButton = new Button(strings.getString("cancel_button_text"));
 		cancelButton.setOnAction(e -> {
 			((Node) e.getSource()).getScene().getWindow().hide();
 		});
-
-		// button box
-		HBox buttons = new HBox();
-		buttons.setSpacing(30);
-		buttons.setPadding(new Insets(30, 30, 30, 30));
-		buttons.getChildren().addAll(addDriver, cancelButton);
-
-		GridPane pane = new GridPane();
-		pane.add(labelBox, 0, 0);
-		pane.add(driverNameBox, 0, 1);
-		pane.add(driversLicenseBox, 0, 2);
-		pane.add(hazardousInfoBox, 0, 3);
-		pane.add(buttons, 0, 4);
-		
-		modalPane.setBottom(pane);
-
+		return cancelButton;
 	}
 
 	private boolean canDriveHaz() {
