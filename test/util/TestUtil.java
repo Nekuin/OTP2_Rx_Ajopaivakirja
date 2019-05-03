4a924abad44106c12536f35f4909cd2aa6b40c35
Jori Nordlund <jori.nordlund@metropolia.fi>
2019-05-03 20:37:41 +0300

    Comment added to TestUtil

--- test/util/TestUtil.java
+++ test/util/TestUtil.java
@@ -1,5 +1,9 @@
 package util;
 
 public class TestUtil {
+	/**
+	 * Use "false" for testing
+	 * REMEMBER TO CHANGE TO "true" BEFORE PUSHING!!!!!!!!!!!
+	 */
 	public static boolean testVersion = true;
 }
